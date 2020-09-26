d30f47e51ca4353814a631bf65eb7402b349a4ab
xuting <1216441996@qq.com>
2020-09-26 19:41:29 +0800

    优化

--- src/main/java/com/xuting/contoller/DockerController.java
+++ src/main/java/com/xuting/contoller/DockerController.java
@@ -1,5 +1,6 @@
 package com.xuting.contoller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @description:
  **/
 @RestController
+@Slf4j
 public class DockerController {
-//    static Log log = LogFactory.getLog(DockerController.class);
 
     @RequestMapping("/")
     public String index() {
-//        log.info("Hello Docker!");
+        log.info("Hello Docker!");
         return "Hello Docker!";
     }
 }
